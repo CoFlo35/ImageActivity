2418d3e4009b6780bdf196a314b0a61bd54fbfee
CoFlo35 <43580764+CoFlo35@users.noreply.github.com>
2021-09-29 20:53:51 -0400

    Implementation of OnItemClickListener to update the Text and Image views when the user selects a picture from the grid, assignment complete

--- app/src/main/java/edu/temple/androidlabsubmission_3/ImageActivity.kt
+++ app/src/main/java/edu/temple/androidlabsubmission_3/ImageActivity.kt
@@ -3,10 +3,15 @@ package edu.temple.androidlabsubmission_3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class ImageActivity : AppCompatActivity() {
+    //val displayText = findViewById<TextView>(R.id.my_displayTextView)
+    //val displayImage = findViewById<ImageView>(R.id.my_displayImageView)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,18 +21,28 @@ class ImageActivity : AppCompatActivity() {
 
         //instantiate custom adapter
         val adapter = ImageAdapter(images)
-        //instantiate a GridLayoutManager, span 2
-        val gridLayout =GridLayoutManager(this,2)
+        //instantiate a GridLayoutManager, span 3
+        val gridLayout =GridLayoutManager(this,3)
         //set the GridLayoutManager to the recycerView
         my_recyclerView.layoutManager = gridLayout
         //set the adapter to the recyclerView
         my_recyclerView.adapter = adapter
+        //create the onclickListner for the adapter
+        adapter.setOnItemClickListener(object:ImageAdapter.onItemCickListener{
+            //implement methods and override to dictate what happens
+            override fun onItemClick(position: Int) {
+                //grab both the local Text and Image display views and change their text and resource values
+                //when the user clicks on an item
+                val textView = findViewById<TextView>(R.id.my_displayTextView)
+                val imageView = findViewById<ImageView>(R.id.my_displayImageView)
+                textView.text = images.get(position).description
+                imageView.setImageResource(images.get(position).resourceID)
+            }
+
+        })
     }
 
-    public fun View.OnClickListener(){
 
-
-    }
 
 
     private fun setDataInList(): ArrayList<ImageObject> {
--- app/src/main/java/edu/temple/androidlabsubmission_3/ImageAdapter.kt
+++ app/src/main/java/edu/temple/androidlabsubmission_3/ImageAdapter.kt
@@ -4,14 +4,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.view.*
 
-class ImageAdapter(val items:ArrayList<ImageObject>):RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+class ImageAdapter(val items:ArrayList<ImageObject> ):RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+
 
     //Inflate the view to be places into the RecyclerView
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.grid_item_view,parent,false)
-        return ViewHolder(view)
+        //return the view and the local listener
+        return ViewHolder(view,mListener)
     }
 
     //Bind the image Resource and Description from the specified index of the ArrayList to the
@@ -19,6 +23,7 @@ class ImageAdapter(val items:ArrayList<ImageObject>):RecyclerView.Adapter<ImageA
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.image.setImageResource(items.get(position).resourceID)
         holder.description = items.get(position).description
+
 
     }
 
@@ -28,8 +33,28 @@ class ImageAdapter(val items:ArrayList<ImageObject>):RecyclerView.Adapter<ImageA
     }
 
     //create the ViewHolder to be used by the Adapter
-    class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+    class ViewHolder(view: View, listener:onItemCickListener):RecyclerView.ViewHolder(view){
         val image: ImageView = itemView.findViewById(R.id.gridImageView)
         lateinit var description:String
+
+        //set the onClickListener to each specific View
+        init{
+            itemView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
+        }
     }
+    //variable is set to the implemented interface
+    private lateinit var mListener:onItemCickListener
+
+    //create an Interface to recieve the adapter position of the item clicked
+    interface onItemCickListener{
+        fun onItemClick(position:Int)
+    }
+
+    //set local listener to the given provided listener
+    fun setOnItemClickListener(listener:onItemCickListener){
+        mListener = listener
+    }
+
 }
